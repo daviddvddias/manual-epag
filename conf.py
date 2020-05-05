2ba5e1d8d7a7eb31ca048a22f171c05bdb3db7bd
Carlos Vieira <edu.carlos.vieira@gmail.com>
2020-05-05 11:32:21 -0300

    Revert "Adiciona biblioteca para vídeos do youtube"
    This reverts commit 21f1c777cafeafba12b36bd32d700f681fbfc226.

--- conf.py
+++ conf.py
@@ -42,7 +42,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx.ext.imgconverter',
-    'sphinxcontrib.youtube',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
